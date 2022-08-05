b8017bc5f469e5a139e82abeb5ba1c02042d58cf
MD MAHADI HASAN <99705111+mahadiinsta@users.noreply.github.com>
2022-08-06 00:46:45 +0600

    until video 33

---
+++ data structure & algo/Misc Problems/climbingStaircase.js
@@ -0,0 +1,7 @@
+function climningStaircase(n){
+    const noOfWays = [1,2];
+    for(let i = 2; i<= n; i++){
+        noOfWays[i] = noOfWays[i -1] + noOfWays[i - 2];
+    }
+    return noOfWays[n -1];
+}
